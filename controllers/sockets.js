0c07073517c2a9b2081e71aa3761b50deb77143c
Krishna Kumar <kkbhagat20@gmail.com>
2025-09-15 01:19:39 +0530

    create join-session listen event

--- controllers/sockets.js
+++ controllers/sockets.js
@@ -3,13 +3,89 @@ const Session = require("../models/session");
 const webRTCSignalingSocket = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+  
 
+    socket.on("prepare-session", async ({ sessionId, userId }) => {
+      console.log(`User ${userId} is preparing to join session ${sessionId}`);
+
+      const session = await Session.findOne({ sessionId });
+
+      if (session) {
+        socket.join(sessionId);
+        console.log(`User ${userId} is observing session ${sessionId}`);
+
+        socket.emit("session-info", {
+          participants: session?.participants,
+        });
+        socket.on("disconnect", async () => {
+          console.log(
+            `Observer ${userId} disconnected from session ${sessionId}`
+          );
+        });
+      } else {
+        console.log(`Session with ID ${sessionId} not found`);
+        socket.emit("error", { message: "Session not found" });
+      }
+    });
    
+     socket.on(
+      "join-session",
+      async ({ sessionId, userId, name, photo, micOn, videoOn }) => {
+        console.log(
+          `User ${name} (${userId}) is attempting to join session ${sessionId}`
+        );
+
+        const session = await Session.findOne({ sessionId });
+        if (session) {
+          const existingParticipantIndex = session.participants.findIndex(
+            (p) => p.userId === userId
+          );
+
+          if (existingParticipantIndex !== -1) {
+            session.participants[existingParticipantIndex] = {
+              ...session.participants[existingParticipantIndex],
+              name: name || session.participants[existingParticipantIndex].name,
+              photo:
+                photo || session.participants[existingParticipantIndex].photo,
+              micOn: micOn,
+              videoOn: videoOn,
+              socketId: socket.id,
+            };
+          } else {
+            const participant = {
+              userId,
+              name,
+              photo,
+              socketId: socket.id,
+              micOn: micOn,
+              videoOn: videoOn,
+            };
+            session.participants.push(participant);
+          }
+          await session.save();
+          socket.join(sessionId);
+
+          console.log(
+            `User ${name} (${userId}) has joined session ${sessionId}`
+          );
+
+          io.to(sessionId).emit(
+            "new-participant",
+            session.participants?.find((i) => i.userId === userId)
+          );
+          io.to(sessionId).emit("session-info", {
+            participants: session.participants,
+          });
+        } else {
+          console.log(`Session with ID ${sessionId} not found`);
+          socket.emit("error", { message: "Session not found" });
+        }
+      }
+    );
 
 
 
 
-    
   });
 };
 
